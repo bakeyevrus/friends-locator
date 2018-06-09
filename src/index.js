b49f60f879c7cdc1011b0e2f78c2cef65360dae2
Ruslan Bakeyev <bakeyevrus@gmail.com>
2018-06-09 21:50:29 +0200

    Fix eslint to support ES6+ syntax

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto/index.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/* eslint-disable */
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
